fe81ed87ac9d9b79a1ab756d39ceadf320ab5e5b
sandeep kumar <7038078+sandeepkumar0153@users.noreply.github.com>
2023-03-17 00:48:40 +0530

    Update cpanel.py

--- cpanel.py
+++ cpanel.py
@@ -1,30 +1,17 @@
 from http.server import HTTPServer
-from sqlalchemy import create_engine
 import os
 import socket
 import sys
 import argparse
 
 host = 'localhost'
-data_payload = 3001
+data_payload = 3000
 backlog = 5
 
-db_url = os.getenv('DATABASE_URL', 'postgresql://localhost:5432/mydatabase')
-# DATABASE_URL uses postgres:// but SQLAlchemy only accepts postgresql://
-db_url = db_url.replace('postgres://', 'postgresql://')
 
 # Get port number from the PORT environment varaible or 3000 if not specified
 port = os.getenv('PORT', 3000)
 
-server = HTTPServer(('0.0.0.0', port), MyServer)
-server.serve_forever()
-
-engine = create_engine(db_url)
-...
-
-with engine.connect() as connection:
-    result = connection.execute(...)
-...
 
 def start_app(port):
     """ A simple echo client """ 
